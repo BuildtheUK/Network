35c49b8c099795da920543d38df705386e026aba
LM-Wolfert <laurens.wolfert@gmail.com>
2026-03-15 19:23:03 +0100

    Unwanted files.

--- src/main/java/net/bteuk/network/building_counter/Building.java
+++
@@ -1,6 +0,0 @@
-package net.bteuk.network.building_counter;
-
-import org.bukkit.Location;
-
-public record Building(int buildingId, Location coordinate, String playerId, int coordinateId) {
-}
--- src/main/java/net/bteuk/network/utils/plotsystem/ReviewCategoryFeedback.java
+++
@@ -1,4 +0,0 @@
-package net.bteuk.network.utils.plotsystem;
-
-public record ReviewCategoryFeedback(ReviewCategory category, ReviewSelection selection, int bookId) {
-}
